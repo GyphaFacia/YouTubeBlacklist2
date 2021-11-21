43b968427d65e0efc6055c0e4fc0f46fa11e67a5
Blank <holoparrentattachment@gmail.com>
2021-11-22 00:48:35 +0300

    Splitted menu to another file

---
+++ ExtensionMenu.js
@@ -0,0 +1,304 @@
+// 888b     d888                            
+// 8888b   d8888                            
+// 88888b.d88888                            
+// 888Y88888P888  .d88b.  88888b.  888  888 
+// 888 Y888P 888 d8P  Y8b 888 "88b 888  888 
+// 888  Y8P  888 88888888 888  888 888  888 
+// 888   "   888 Y8b.     888  888 Y88b 888 
+// 888       888  "Y8888  888  888  "Y88888 
+class ExtensionMenu extends ExtensionElement{
+	getNextSiblingSelector(){return '#container #buttons > ytd-button-renderer'}
+	first(){
+		this.tag = 'aside'
+		this.className = 'ytbl-extension-menu hidden'
+		this.activeTab = 0
+		this.loadOptions()
+	}
+	
+	onRender(){
+		if(window.location.href.includes('/watch')){
+			// page with video player has no header border for some reason
+			this.DOM.style.top = '56px' 
+		}
+		
+		this.updateMenu(true)
+	}
+	
+	deactivateTabs(){
+		for(let tab of document.querySelectorAll('.menu-tabs__tab')){
+			tab.classList.add('hidden')
+		}
+		for(let switcher of document.querySelectorAll('.menu-switchers__switcher')){
+			switcher.classList.remove('active')
+		}
+	}
+	
+	restoreActiveTab(){
+		this.deactivateTabs()
+		let switchers = document.querySelectorAll('.menu-switchers__switcher')
+		let tabs = document.querySelectorAll('.menu-tabs__tab')
+		switchers[this.activeTab].classList.add('active')
+		tabs[this.activeTab].classList.remove('hidden')
+	}
+	
+	hookTabSwitchers(){
+		let switchers = document.querySelectorAll('.menu-switchers__switcher')
+		let tabs = document.querySelectorAll('.menu-tabs__tab')
+		for (let i = 0; i < switchers.length; i++) {
+			switchers[i].onclick = ()=>{
+				this.deactivateTabs()
+				switchers[i].classList.add('active')
+				tabs[i].classList.remove('hidden')
+				this.activeTab = i
+			}
+		}
+	}
+	
+	////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////Blacklist//////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	getTabBlacklistCode(){
+		let code = ''
+		for(let channel in banlist.content){
+			code += `
+			<span class="banned-channel">
+		        <a class="banned-channel__name" href="${banlist.content[channel]}">${channel}</a>
+		        <div class="banned-channel__button">UnHide Channel</div>
+		    </span>
+			`
+		}
+		return code
+	}
+	
+	hookTabBlacklist(){
+		for(let button of document.querySelectorAll('.banned-channel__button')){
+			button.onclick = ()=>{
+				banlist.removeFromList(button.parentNode.children[0].innerText)
+			}
+		}
+	}
+	
+	////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////Suggestions///////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	getTabSuggestionsCode(){
+		let code = ''
+		for(let link in suggestions.content){
+			let title = suggestions.content[link]
+			title = title.length < 30 ? title : (title.substr(0, 27) + '...')
+			code += `
+			<span class="banned-video">
+		        <a class="banned-video__name" href="${link}">${title}</a>
+		        <div class="banned-video__button ">UnHide Video</div>
+		    </span>
+			`
+		}
+		return code
+	}
+	
+	hookTabSuggestions(){
+		for(let button of document.querySelectorAll('.banned-video__button')){
+			button.onclick = ()=>{
+				let link = button.parentNode.children[0].href
+				suggestions.removeFromList(link)
+			}
+		}
+	}
+	
+	////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////Options//////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	loadOptions(){
+		this.options = JSON.parse(localStorage.getItem('options'))
+		if(!this.options){
+			this.options = {
+				'HideBanned': true,
+				'HideWatched' : true,
+				'HideSuggested': true,
+			}
+		}
+		this.saveOptions()
+	}
+	
+	saveOptions(){
+		localStorage.setItem('options', JSON.stringify(this.options))
+	}
+	
+	getOptionsDescriptions(){
+		return {
+			'HideBanned': 'Hide videos from blacklisted channels',
+			'HideWatched': 'Hide watched videos',
+			'HideSuggested': 'Hide videos you blacklisted from suggestions',
+		}
+	}
+	
+	getOptionByDescription(description){
+		let kv = this.getOptionsDescriptions()
+		for(let k in kv){
+			if(kv[k] == description){
+				return k
+			}
+		}
+		return ''
+	}
+	
+	////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////Settings//////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	getTabSettingsCode(){
+		let kv = this.getOptionsDescriptions()
+		
+		let code = ''
+		for(let k in kv){
+			let mark = this.options[k] ? '✔' : ' '
+			let v = kv[k]
+			code += `
+			<div class="extension-settings-item">
+	            <span class="extension-settings-item__name">${v}</span>
+				<button class="extension-settings-item__checkbox">${mark}</button>
+	        </div>  
+			` 
+		}
+		
+		code = `<div class="extension-settings"> ${code} </div>`
+		
+		return code
+	}
+	
+	hookTabSettings(){
+		for(let button of document.querySelectorAll('.extension-settings-item__checkbox')){
+			button.onclick = ()=>{
+				let optionsDescr = button.parentNode.children[0].innerText
+				let option = this.getOptionByDescription(optionsDescr)
+				this.options[option] = !this.options[option]
+				this.saveOptions()
+				this.updateMenu()
+			}
+		}
+	}
+	
+	////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////About///////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	getTabAboutCode(){
+		return `
+		<div class="about-links">
+			<button class="about-links__clearbtn">Clear Blacklists</button>
+			
+			<a class="about-links__exportbtn">Export Blacklists</a>
+			
+			<span class="about-links-import-wrapper">
+				<input class="about-links__importbtn" type="file">
+				Import Blacklists
+			</span>
+			
+			<a href="https://github.com/GyphaFacia" class="about-links__link">Visit my GitHub</a>
+	    </div>
+		`
+	}
+	// <input type="file" name="" value="">
+	
+	hookTabAbout(){
+		let data = {}
+		data.blacklist = JSON.parse(localStorage.getItem('blacklist'))
+		data.suggestions = JSON.parse(localStorage.getItem('suggestions'))
+		data = JSON.stringify(data);
+		data = "data:text/json;charset=utf-8," + encodeURIComponent(data)
+		
+		// clear
+		let clearBtn = document.querySelector('.about-links__clearbtn')
+		clearBtn.onclick = ()=>{
+			if(confirm('Do you actually want to reset your blacklists for this extension ?')){
+				localStorage.setItem('blacklist', '{}')
+				localStorage.setItem('suggestions', '{}')
+				
+				banlist.update()
+				suggestions.update()
+				setStoreIsUpdated(true)
+			}
+		}
+		
+		// export
+		let exportBtn = document.querySelector('.about-links__exportbtn')
+		exportBtn.setAttribute("href", data)
+		exportBtn.setAttribute("download", "YoutubeBlacklists.json")
+		
+		// import
+		let importBtn = document.querySelector('.about-links-import-wrapper')
+		importBtn.onclick = ()=>{
+			importBtn.children[0].click()
+		}
+		importBtn.children[0].onchange = (e)=>{
+			let reader = new FileReader();
+			reader.readAsText(e.target.files[0])
+			reader.onload = ()=>{
+				let data = reader.result
+				data = JSON.parse(data);
+				
+				if('blacklist' in data && 'suggestions' in data){
+					localStorage.setItem('blacklist', JSON.stringify(data.blacklist))
+					localStorage.setItem('suggestions', JSON.stringify(data.suggestions))
+					
+					banlist.update()
+					suggestions.update()
+					setStoreIsUpdated(true)
+					alert('Blacklists successfully imported')
+				}
+				else{
+					alert('Error occurred reading imported file')
+				}
+			}
+			
+			reader.onerror = ()=>{
+				alert('Error occurred reading imported file')
+			}
+		}
+	}
+	
+	////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	updateMenu(startHidden = false){
+		this.innerHTML = `
+		<div class="menu-switchers">
+			<div class="menu-switchers__switcher active">Black List</div>
+			<div class="menu-switchers__switcher">Suggestions</div>
+			<div class="menu-switchers__switcher">Settings</div>
+			<div class="menu-switchers__switcher">About</div>
+		</div>
+		
+		<div class="menu-tabs">
+            <div class="menu-tabs__tab">${this.getTabBlacklistCode()}</div>
+			<div class="menu-tabs__tab hidden">${this.getTabSuggestionsCode()}</div>
+            <div class="menu-tabs__tab hidden">${this.getTabSettingsCode()}</div>
+            <div class="menu-tabs__tab hidden">${this.getTabAboutCode()}</div>
+        </div>
+		`
+		
+		this.hookTabSwitchers()		
+		this.hookTabBlacklist()
+		this.hookTabSuggestions()
+		this.hookTabSettings()
+		this.hookTabAbout()
+		
+		this.restoreActiveTab()
+		
+		if(startHidden){
+			this.DOM.classList.add('hidden')
+		}
+		else{
+			this.DOM.classList.remove('hidden')
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
